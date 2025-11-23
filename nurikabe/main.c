d15c9cb4268ac519c963db989bc51d4b36a7aba1
rphii <raphael.seitz18@gmail.com>
2025-11-23 22:22:49 +0000

    replace nue with nurikabe

--- nurikabe/main.c
+++ nurikabe/main.c
@@ -1,18 +1,18 @@
 #include <rltui.h>
 
-typedef struct Nue {
+typedef struct Nurikabe {
     Tui_Sync sync;
     Tui_Core_Callbacks callbacks;
     struct Tui_Core *tui;
-} Nue;
+} Nurikabe;
 
-bool nue_input(Tui_Input *input, bool *flush, void *user) {
-    Nue *nue = user;
+bool nurikabe_input(Tui_Input *input, bool *flush, void *user) {
+    Nurikabe *nurikabe = user;
     switch(input->id) {
         case INPUT_TEXT: {
             switch(input->text.val) {
                 case 'q': {
-                    tui_core_quit(nue->tui);
+                    tui_core_quit(nurikabe->tui);
                 } break;
             }
         } break;
@@ -21,30 +21,30 @@ bool nue_input(Tui_Input *input, bool *flush, void *user) {
     return false;
 }
 
-bool nue_update(void *user) {
+bool nurikabe_update(void *user) {
     return false;
 }
 
-void nue_render(Tui_Buffer *buffer, void *user) {
+void nurikabe_render(Tui_Buffer *buffer, void *user) {
 }
 
-void nue_resized(Tui_Point size, Tui_Point pixels, void *user) {
+void nurikabe_resized(Tui_Point size, Tui_Point pixels, void *user) {
 }
 
 int main(void) {
 
-    Nue nue = {
-        .callbacks.input = nue_input,
-        .callbacks.update = nue_update,
-        .callbacks.render = nue_render,
-        .callbacks.resized = nue_resized,
+    Nurikabe nurikabe = {
+        .callbacks.input = nurikabe_input,
+        .callbacks.update = nurikabe_update,
+        .callbacks.render = nurikabe_render,
+        .callbacks.resized = nurikabe_resized,
         .tui = tui_core_new(),
     };
 
     tui_enter();
-    tui_core_init(nue.tui, &nue.callbacks, &nue.sync, &nue);
-    while(tui_core_loop(nue.tui)) {}
-    tui_core_free(nue.tui);
+    tui_core_init(nurikabe.tui, &nurikabe.callbacks, &nurikabe.sync, &nurikabe);
+    while(tui_core_loop(nurikabe.tui)) {}
+    tui_core_free(nurikabe.tui);
     tui_exit();
 
     return 0;
